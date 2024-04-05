778d6f61c615207f8228d3b123a7c6614d43e495
Hakan Göktürk <hakangktrk@gmail.com>
2024-04-05 16:09:25 +0300

    feat: Api Adjustment

--- app/src/main/java/com/hakangokturk/mymovie/network/MovieApi.kt
+++ app/src/main/java/com/hakangokturk/mymovie/network/MovieApi.kt
@@ -1,21 +1,15 @@
 package com.hakangokturk.mymovie.network
 
-import com.hakangokturk.mymovie.database.entity.User
 import com.hakangokturk.mymovie.model.GenreList
 import com.hakangokturk.mymovie.model.Movie
 import com.hakangokturk.mymovie.model.MovieCredits
 import com.hakangokturk.mymovie.model.NowPlaying
-import com.hakangokturk.mymovie.model.RefreshToken
 import com.hakangokturk.mymovie.model.Upcoming
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
-
-    @GET("3/discover/movie")
-    suspend fun getMovie(): Movie
 
     @GET("3/discover/movie")
     suspend fun getMovieById(
